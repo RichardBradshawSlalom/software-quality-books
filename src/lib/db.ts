935427e43ce619635d84975ee8a7cdcd22d0d4d1
alifung-slalom <alison.fung@slalom.com>
2025-02-05 14:17:53 +0000

    add logs to middleware and prisma db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,39 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'event',
+        level: 'error',
+      },
+      {
+        emit: 'event',
+        level: 'info',
+      },
+      {
+        emit: 'event',
+        level: 'warn',
+      },
+    ],
+  })
+
+  prisma.$on('query', (e) => {
+    console.log('Query: ' + e.query)
+    console.log('Params: ' + e.params)
+    console.log('Duration: ' + e.duration + 'ms')
+  })
+
+  return prisma
 }
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  console.log(`[Middleware] ${req.method} ${req.nextUrl.pathname} - Request received`)
+  console.log(`Request body: ${req.body}`)
+  console.log(`Request headers: ${req.headers}`)
+  const response = NextResponse.next()
+  console.log(`Response body: ${response.body}`)
+  return response
+}
 
 export default withAuth({
   pages: {
     signIn: '/login',
-  }
+  },
 })
 
 export const config = {
-  matcher: [
-    '/books/new',
-    '/books/edit/:path*',
-    '/api/books/:path*/edit',
-    '/api/books/new',
-  ]
-} 
+  matcher: ['/books/new', '/books/edit/:path*', '/api/books/:path*/edit', '/api/books/new'],
+}
